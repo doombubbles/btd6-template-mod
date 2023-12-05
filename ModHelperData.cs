b7810a29c3554680b2a47d8cdb7a24ffd6031207
doombubbles <obsidiantoolzg@gmail.com>
2023-12-05 15:10:56 -0800

    Update default WorksOnVersion

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "39.0";
+    public const string WorksOnVersion = "40.0";
     public const string Version = "1.0.0";
     public const string Name = "TemplateMod";
 
